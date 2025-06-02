a5c8c134f7bd7fc646ee4e3266412ba3eb32902f
Michael-Ralph <MichaelR@praxima.com>
2025-06-02 07:53:14 +0200

    file structure and main.go set up

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/tasks", handlers.GetTask)
+	r.GET("/tasks/:id", handlers.GetTaskByID)
+	r.POST("/tasks", handlers.CreateTask)
+	r.PUT("/tasks/:id", handlers.UpdateID)
+	r.DELETE("/tasks/:id", handlers.DeleteTask)
+
+	r.Run("localhost:9000")
+}
